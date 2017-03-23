12f21183f3504d8a7aabe58c371c9620bd92a4d1
Chan chih-yu <zan168577@gmail.com>
2017-03-23 15:54:42 +0800

    cake

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-
+FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+});
+let day=0,mon=0;
 
 $(function() {
+day=$("#daybox").val();
+mon=$("#monbox").val();
   $("#start").click(function(){
       FB.api(
       '/approprie/posts',
       'GET',
-      {"since":"2017-03-22 09:59:00","limit":"1"},
+      {"since":"2017-"+mon+"-"+day+" 09:59:00","limit":"1"},
       function(response) {
       // Insert your code here
         console.log(response);
       }
     );
   });
+
+
 });
 

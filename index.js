5c8c78f61257c1d58768a5038c0f64c99fee3585
zan168577 <zan168577>
2017-03-24 00:17:31 +0800

    '55'

--- index.js
+++ index.js
@@ -5,21 +5,39 @@ FB.getLoginStatus(function (response) {
 $(function () {
 
   $("#start").click(function () {
-contentSearch();
-    
+    contentSearch();
+
   });
-function contentSearch(){
-  FB.api(
+
+  function contentSearch() {
+    FB.api(
       '/approprie/posts',
       'GET', {
         "since": "2017-03-22 09:59:00",
         "limit": "1"
       },
       function (response) {
-        let textid=response.data[0].id;
-        console.log(textid,response);
+        let textid = response.data[0].id;
+        console.log(textid, response);
+        contentComment(textid);
       }
     );
-}
+  }
+
+  function contentComment(conid) {
+    let apii = '/' + conid + '/comment';
+    FB.api(
+      apii,
+      'POST', {
+        "message": "+1"
+      },
+      function (response) {
+        // Insert your code here
+        if(response.id){
+          alert('留言成功!');
+        }
+      }
+    );
+  }
 
 });

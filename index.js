30e7eb08ee2f2ddbbbcbd103eb38c4d70ba567d4
zan168577 <zan168577>
2017-03-24 00:59:59 +0800

    '55'

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ FB.getLoginStatus(function (response) {
 });
 
 $(function () {
-
+var token="EAACEdEose0cBAIeXeJduzVpKPqHJo5B6UnfLsfBcDZC95q87lZCeCtY2vlZBTiNUgBo5pCQIyAquS2AD6nQGbEb3w8Kq2pgwjNbrwZCoDkKW4gqjVIFrN145qsu27pRyNmLZAi8IUAePZABYkj0qOY7WplM6d03pDJ7zfTZAhFRLGFUdMcLLmCqim0feMJXMnMZD";
   $("#start").click(function () {
     contentSearch();
 
@@ -14,12 +14,13 @@ $(function () {
       '/approprie/posts',
       'GET', {
         "since": "2017-03-22 09:59:00",
-        "limit": "1"
+        "limit": "1",
+        "access_token":token
       },
       function (response) {
         let textid = response.data[0].id;
         console.log(textid, response);
-        contentComment(textid);
+        // contentComment(textid);
       }
     );
   }

12db755f2a94efcab2847979fb0187b72fc81fed
zan168577 <zan168577>
2017-03-23 23:25:09 +0800

    '123'

--- index.js
+++ index.js
@@ -1,29 +1,3 @@
-window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '1845583559041249',
-      xfbml      : true,
-      version    : 'v2.8',
-      status     :true
-    });
-    FB.AppEvents.logPageView();
-  };
-
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "//connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
-
-   (function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/zh_TW/sdk.js#xfbml=1&version=v2.8&appId=1845583559041249";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
-
 FB.getLoginStatus(function(response) {
     statusChangeCallback(response);
 });

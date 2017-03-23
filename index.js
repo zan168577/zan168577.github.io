7cf9c1df51d60595dd07fa78c538db3700700017
Chan chih-yu <zan168577@gmail.com>
2017-03-23 15:57:24 +0800

    cake

--- index.js
+++ index.js
@@ -6,11 +6,12 @@ let day=0,mon=0;
 $(function() {
 day=$("#daybox").val();
 mon=$("#monbox").val();
+let date="2017-"+mon+"-"+day+" 09:59:00";
   $("#start").click(function(){
       FB.api(
       '/approprie/posts',
       'GET',
-      {"since":"2017-"+mon+"-"+day+" 09:59:00","limit":"1"},
+      {"since":date,"limit":"1"},
       function(response) {
       // Insert your code here
         console.log(response);

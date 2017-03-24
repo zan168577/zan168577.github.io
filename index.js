d53677401d189380774f4de70764c56d0aa3122a
Chan chih-yu <zan168577@gmail.com>
2017-03-24 13:04:50 +0800

    1

--- index.js
+++ index.js
@@ -1,47 +1,49 @@
-FB.getLoginStatus(function (response) {
-  statusChangeCallback(response);
+FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
 });
 
-$(function () {
-var token="";
+$(function() {
+    var token = "";
 
-  $("#start").click(function () {
-    token=$("#tokenbox").val();
-    contentSearch();
+    $("#start").click(function() {
+        token = $("#tokenbox").val();
+        contentSearch();
 
-  });
+    });
 
-  function contentSearch() {
-    FB.api(
-      '/approprie/posts',
-      'GET', {
-        "since": "2017-03-22 09:59:00",
-        "limit": "1",
-        "access_token":token
-      },
-      function (response) {
-        let textid = response.data[0].id;
-        console.log(textid, response);
-        contentComment(textid);
-      }
-    );
-  }
+    function contentSearch() {
+        FB.api(
+            '/approprie/posts',
+            'GET', {
+                "since": "2017-03-22 09:59:00",
+                "limit": "1",
+                "access_token": token
+            },
+            function(response) {
+                let textid = response.data[0].id;
+                console.log(textid, response);
+                // contentComment(textid);
+            }
+        );
+    }
 
-  function contentComment(conid) {
-    let apii = '/' + conid + '/comments';
-    FB.api(
-      apii,
-      'POST', {
-        "message": "+1",
-        "access_token":token
-      },
-      function (response) {
-        // Insert your code here
-        if(response.id){
-          alert('留言成功!');
-        }
-      }
-    );
-  }
+    function contentComment(conid) {
+        let apii = '/' + conid + '/comments';
+        FB.api(
+            apii,
+            'POST', {
+                "message": "+1",
+                "access_token": token
+            },
+            function(response) {
+                // Insert your code here
+                if (response.id) {
+                    alert('留言成功!');
+                } else {
+                    console.log('再次嘗試');
+                }
+            }
+        );
+    }
 
 });
